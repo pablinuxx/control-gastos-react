16dc73dccbde36e39a0cbf98f0a3b2e3ef70eaab
Pachin <p.sarasua@hotmail.com>
2023-07-06 22:25:45 -0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import NuevoPresupuesto from "./NuevoPresupuesto";
+export const Header = () => {
+  return (
+    <header>
+      <h1>Planificador de Gastos</h1>
+      <NuevoPresupuesto />
+    </header>
+  );
+};
