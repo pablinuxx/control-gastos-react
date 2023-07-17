63c6e1176fcf39af3123b0263192558c943c5ed5
Pachin <p.sarasua@hotmail.com>
2023-07-17 14:22:34 -0300

    cambio en el estilo del gasto

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   LeadingActions,
   SwipeableList,
@@ -6,7 +7,9 @@ import {
   TrailingActions,
 } from "react-swipeable-list";
 import "react-swipeable-list/dist/styles.css";
+
 import { formatearFecha } from "../helpers";
+
 import IconoAhorro from "../img/icono_ahorro.svg";
 import IconoCasa from "../img/icono_casa.svg";
 import IconoComida from "../img/icono_comida.svg";
@@ -14,7 +17,8 @@ import IconoGastos from "../img/icono_gastos.svg";
 import IconoOcio from "../img/icono_ocio.svg";
 import IconoSalud from "../img/icono_salud.svg";
 import IconoSuscripciones from "../img/icono_suscripciones.svg";
-const diccionarioCategoria = {
+
+const diccionarioIconos = {
   ahorro: IconoAhorro,
   comida: IconoComida,
   casa: IconoCasa,
@@ -23,20 +27,24 @@ const diccionarioCategoria = {
   salud: IconoSalud,
   suscripciones: IconoSuscripciones,
 };
+
 const Gasto = ({ gasto, setGastoEditar, eliminarGasto }) => {
-  const { categoria, nombre, cantidad, fecha, id } = gasto;
+  const { categoria, nombre, cantidad, id, fecha } = gasto;
+
   const leadingActions = () => (
     <LeadingActions>
       <SwipeAction onClick={() => setGastoEditar(gasto)}>Editar</SwipeAction>
     </LeadingActions>
   );
+
   const trailingActions = () => (
     <TrailingActions>
-      <SwipeAction destructive={true} onClick={() => eliminarGasto(id)}>
+      <SwipeAction onClick={() => eliminarGasto(id)} destructive={true}>
         Eliminar
       </SwipeAction>
     </TrailingActions>
   );
+
   return (
     <SwipeableList>
       <SwipeableListItem
@@ -45,12 +53,13 @@ const Gasto = ({ gasto, setGastoEditar, eliminarGasto }) => {
       >
         <div className="gasto sombra">
           <div className="contenido-gasto">
+            <img src={diccionarioIconos[categoria]} alt="Icono Gasto" />
             <div className="descripcion-gasto">
-              <img src={diccionarioCategoria[categoria]} alt="icono Gasto" />
               <p className="categoria">{categoria}</p>
               <p className="nombre-gasto">{nombre}</p>
               <p className="fecha-gasto">
-                Agregado el {""} <span>{formatearFecha(fecha)}</span>
+                Agregado el: {""}
+                <span>{formatearFecha(fecha)}</span>
               </p>
             </div>
           </div>
